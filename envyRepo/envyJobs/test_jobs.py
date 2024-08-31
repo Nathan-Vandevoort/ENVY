8f8ffd4de24926bd1f973d765154ee18f5bbeffc
Nathan.v0602 <nathan.v.2021@gmail.com>
2024-08-31 15:59:20 -0700

    Completely changed the project organization

---
+++ envyRepo/envyJobs/test_jobs.py
@@ -0,0 +1,17 @@
+import prep_env
+import job
+from enums import Purpose as p
+from datetime import datetime
+
+test_job = job.Job(f"potato - {datetime.now().strftime('%d-%m-%Y %H-%M-%S')}")
+test_job.add_range(1, 150, 1)
+test_job.set_meta()
+test_job.set_allocation(1)
+test_job.set_purpose(p.CACHE)
+test_job.set_type('PLUGIN_EXAMPLE')
+test_job.set_environment({
+})
+test_job.set_parameters({
+    'A': 5
+})
+test_job.write()
