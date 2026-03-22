5bbbb6f1fa09f2d9ec5a1940a16fed11a8c39972
Nathan <nathan.v.2021@gmail.com>
2026-03-21 17:03:33 -0700

    wip

--- envy/api/client/api.py
+++ envy/api/client/api.py
@@ -13,28 +13,36 @@ from ..data import RPCRequest
 logger = logging.getLogger(__name__)
 
 
+class Client:
+
+    async def echo_task(self, task: Task, message="something") -> Task:
+        return task
+
+
 class RPCClient:
 
     server: Server | None = None
     _model_cache: dict[str, type[BaseModel]] = {}
 
-    async def echo_job_properties(self, job: Job, message="something") -> None:
+    async def echo_job_properties(self, job: Job, message="something") -> str:
         message = self._format_message("echo_job_properties", job, message=message)
+
         return await self._send_message(message)
 
     async def echo_task(self, task: Task, message="something") -> None:
         message = self._format_message("echo_task", task, message=message)
-        return await self._send_message(message)
 
-    def _format_message(self, func_name: str, *args, **kwargs) -> RPCRequest:
-        envelope = RPCRequest(func_name=func_name, args=args, kwargs=kwargs)
-        return envelope
+        return await self._send_message(message)
 
     async def _send_message(self, message: RPCRequest) -> Any:
         if not self.server:
             raise ValueError("a server must be registered before calling the API")
 
         return await self.server.send(message)
+
+    def _format_message(self, func_name: str, *args, **kwargs) -> RPCRequest:
+        envelope = RPCRequest(func_name=func_name, args=args, kwargs=kwargs)
+        return envelope
 
     def register_server(self, server: Server) -> None:
         self.server = server
